9b176b34247cd78dfdd45ca593433dcb52592fb1
Eric Hendrix <agentmrbig@gmail.com>
2019-12-03 01:09:06 -0500

    Updates

---
+++ precache-manifest.293367788059ea01a90f51b47252e05e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f46953e0d3c4adc37adf88bbc103c22",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "cc30a8366d7c849a2cb7",
+    "url": "/MonstersRolodex/static/css/main.c40c376a.chunk.css"
+  },
+  {
+    "revision": "65fba26b38006aac3ac1",
+    "url": "/MonstersRolodex/static/js/2.5cc2dc95.chunk.js"
+  },
+  {
+    "revision": "cc30a8366d7c849a2cb7",
+    "url": "/MonstersRolodex/static/js/main.045cfa3e.chunk.js"
+  },
+  {
+    "revision": "6659864878023d9b7202",
+    "url": "/MonstersRolodex/static/js/runtime-main.ffd63a2a.js"
+  }
+]);
