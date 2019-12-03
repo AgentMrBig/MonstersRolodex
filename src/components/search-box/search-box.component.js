b2b18354af4027eb2761dfbda65678c351836b14
Eric Hendrix <agentmrbig@gmail.com>
2019-12-02 23:20:23 -0500

    extracted out search-box to its own functional component

---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchInput = styled.input`
+  -webkit-appearance: none;
+  border: none;
+  outline: none;
+  padding: 10px;
+  line-height: 30px;
+  margin-bottom: 30px;
+`;
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <SearchInput
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
+);
